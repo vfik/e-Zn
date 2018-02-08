2c60b0d5578e40423b68eb66930928671cab8627
Matt Vuk <matthewvuk@gmail.com>
2018-02-08 12:29:25 -0500

    home template update

--- page-templates/home.php
+++ page-templates/home.php
@@ -41,23 +41,22 @@ endwhile;
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-3">
+		<div class="col-md-3 box">
 
 		</div>
 		<div class="col-md-3">
-			<div class="box">
 
-			</div>
+		</div>
+		<div class="col-md-6">
+			<h2>Benefits of Zinc</h2>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-3">
-			<div class="box">
 
-			</div>
 		</div>
-		<div class="col-md-6">
-			<h2>Benefits of Zinc</h2>
+		<div class="col-md-3 box">
+
 		</div>
 	</div>
 </div>
