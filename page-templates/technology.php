3bfe0bc8f96579a28c940a0a971f8e68de7e3105
Matt Vuk <matthewvuk@gmail.com>
2018-02-08 17:20:33 -0500

    technology innovation

--- page-templates/technology.php
+++ page-templates/technology.php
@@ -29,7 +29,7 @@ endwhile;
 				<div class="right-content">
 					<img src="/wp-content/uploads/2018/02/gf-placeholder.jpg" alt="">
 					<h2>The innovation</h2>
-					<p>Numerous attempts have been made to store energy in zinc metal in past six decades based on an approach where formation, storage and dissolution of zinc metal are conducted in different containers as illustrated in the figure.   None of these attempts was successful due to the problem has been associated with the transportation of solid metal between the different containers.  Jamming tends to occur in pipes, in pumps and in cells.</p>
+					<p>Numerous attempts have been made to store energy in zinc metal in past six decades based on an approach where formation, storage and dissolution of zinc metal are conducted in different containers as illustrated in the figure.</p><p>None of these attempts was successful due to the problem has been associated with the transportation of solid metal between the different containers.  Jamming tends to occur in pipes, in pumps and in cells.</p>
 				</div>
 			</div>
 		</div>
