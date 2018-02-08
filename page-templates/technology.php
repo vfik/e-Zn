eab68dc4655b104da1e80eb75719dd66785bf517
Matt Vuk <matthewvuk@gmail.com>
2018-02-08 17:28:45 -0500

    technology page update

--- page-templates/technology.php
+++ page-templates/technology.php
@@ -23,20 +23,22 @@ endwhile;
 			<div class="col-md-6 left">
 				<div class="left-content">
 					<img src="/wp-content/uploads/2018/02/gf-placeholder.jpg" alt="">
+					<p>label for the image</p>
 				</div>
 			</div>
 			<div class="col-md-6 right">
 				<div class="right-content">
 					<img src="/wp-content/uploads/2018/02/gf-placeholder.jpg" alt="">
 					<h2>The innovation</h2>
-					<p>Numerous attempts have been made to store energy in zinc metal in past six decades based on an approach where formation, storage and dissolution of zinc metal are conducted in different containers as illustrated in the figure.</p><p>None of these attempts was successful due to the problem has been associated with the transportation of solid metal between the different containers.  Jamming tends to occur in pipes, in pumps and in cells.</p>
+					<p>Numerous attempts have been made to store energy in zinc metal in the past siz decades based on an approach featuring multiple containers of zinc undergoing the processes of formation, storage and dissolution.</p>
+					<p>e-Zn abandoned the traditional approach and placed all elements in a single container - eliminating the jamming problems and inventing the e-Zn Reactor. </p>
 				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-6">
-				<h3>e-Zn abandoned the traditional approach of having to transport the zinc metal between different containers</h3>
-				<p>By placing all the elements for zinc formation, dissolution and stage within the same container therefore completely avoided the problem of jamming</p>
+				<h3>The e-Zn reactor is an evolution beyond the battery.</h3>
+				<p>talk about</p>
 			</div>
 		</div>
 	</div>
