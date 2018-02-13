5ea68d70f453f10d569a7b306c24d5250f08f825
Matt Vuk <matthewvuk@gmail.com>
2018-02-13 16:46:49 -0500

    typekit embed code

--- header.php
+++ header.php
@@ -21,6 +21,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
+
+	<link rel="stylesheet" href="https://use.typekit.net/ind6pcp.css">
+
 </head>
 
 <body <?php body_class(); ?>>
