453db983340d7a3e54bd9cff782d5357a9a73a3a
Matt Vuk <matthewvuk@gmail.com>
2018-02-18 15:10:02 -0500

    company timeline

--- page-templates/company.php
+++ page-templates/company.php
@@ -57,6 +57,7 @@ endwhile;
 	</div>
 </div>
 
+<!-- Timeline -->
 <div class="project">
 	<div class="container">
 		<div class="row">
@@ -66,10 +67,81 @@ endwhile;
 			<div class="col-md-6 btf">
 				<div class="bg-three-four">
 					<div class="content">
-						<h2>SDTC project</h2>
+						<h3 class="dateTitle">October 2012</h3>
+						<h2>Company founded</h2>
 						<p>Demonstration of the application feasibility of solar energy storage in zinc metal with Zn-Reactor</p>
-						<p>1 kW/24 hours system. Three industrial partners Canadian solar, newesolar and broy engineering.</p>
-						<p>funding $1.5M and project started October 2016.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="project alt">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="/wp-content/uploads/2018/02/e-zn-solar-panels.jpg" alt="">
+			</div>
+			<div class="col-md-6 btf">
+				<div class="bg-three-four">
+					<div class="content">
+						<h3 class="dateTitle">2013-2016</h3>
+						<h2>Prototype Reactors</h2>
+						<p>extensive development with 8 iterations in design. The first lab was set up and we went to work. The system was developed to be fully automated with electronic boards.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="project">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="/wp-content/uploads/2018/02/e-zn-solar-panels.jpg" alt="">
+			</div>
+			<div class="col-md-6 btf">
+				<div class="bg-three-four">
+					<div class="content">
+						<h3 class="dateTitle">April 2017</h3>
+						<h2>New facility; patents issued</h2>
+						<p>e-Zn upgraded to current office. US and chinese patents have been issued.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="project alt">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="/wp-content/uploads/2018/02/e-zn-solar-panels.jpg" alt="">
+			</div>
+			<div class="col-md-6 btf">
+				<div class="bg-three-four">
+					<div class="content">
+						<h3 class="dateTitle">December 2017</h3>
+						<h2>SDTC Solar Project Installation</h2>
+						<p>e-Zn was awarded grant by Sustainable Devlopment Technology Canada to install solar panels to work directly with the reactors.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="project">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="/wp-content/uploads/2018/02/e-zn-solar-panels.jpg" alt="">
+			</div>
+			<div class="col-md-6 btf">
+				<div class="bg-three-four">
+					<div class="content">
+						<h3 class="dateTitle">January 2018</h3>
+						<h2>20 cell system integrated</h2>
+						<p>the zinc reactors are up and running.</p>
 					</div>
 				</div>
 			</div>
