2d44f05fd098118256f0b74035dd31e2736a0e7e
Matt Vuk <matthewvuk@gmail.com>
2018-02-21 16:51:08 -0500

    update company template

--- page-templates/company.php
+++ page-templates/company.php
@@ -17,7 +17,7 @@ endwhile;
 
 <!-- Begin Page Content -->
 
-<div class="container">
+<!-- <div class="container">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="companyslider company">
@@ -30,6 +30,57 @@ endwhile;
 			<div class="companysub">
 				<div class="content">
 					<h1>A world leading zinc scientist's discovery sparked an innovation in metal energy storage.</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> -->
+
+<div class="company-intro-text">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="intro-text">
+					<h1>e-Zn is a Toronto, Canada, based corporation that was founded in 2012.</h1>
+					<p>The mission of the company is to commercialize energy storage solutions that will make renewable energy such as solar and wind economic, competitive and reliable.</p>
+					<p>Toward that, we have successfully developed a breakthrough electrochemical reactor (Zn Reactor) technology for storing energy in zinc metal.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="technology-spread">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 content">
+				<div class="right-content">
+					<h2>Founder & CEO</h2>
+					<p>Dr. Zhang is a visionary and seasoned technology developer.  Prior to founding e-Zn, Dr. Zhang worked for 20 years in Teck Resources as a scientist and later as manager of R&D leading a team to develop new zinc products for battery.</p>
+					<p>Dr. Zhang is an acknowledged world-expert in zinc electrochemistry and the author of the book “Corrosion and Electrochemistry of Zinc”, which has been the most comprehensive book on the subject since its publication in 1996.</p>
+					<p>Dr. Zhang has over 50 peer-reviewed publications and holds several patents in the field. Dr. Zhang received his PhD in Electrochemistry and Materials Science from the Free University of Brussels and held post-doctoral positions at MIT and Columbia University.</p>
+				</div>
+			</div>
+			<div class="col-md-6 image">
+				<div class="left-content">
+					<img src="/wp-content/uploads/2018/02/gregory-zhang-e-zn.png" alt="">
+				</div>
+			</div>
+		</div>
+		<!--  -->
+		<div class="row">
+			<div class="col-md-6 image">
+				<div class="left-content">
+					<img src="/wp-content/uploads/2018/02/gregory-zhang-e-zn.png" alt="">
+				</div>
+			</div>
+			<div class="col-md-6 content">
+				<div class="right-content">
+					<h2>Facility</h2>
+					<p>Our office and laboratory facility.</p>
+					<p>25 Advance Rd<br>Toronto, Ontario<br>Canada, M8Z 2S6</p>
+					<p>416-000-0000</p>
+					<p>info@e-zn.com</p>
 				</div>
 			</div>
 		</div>
