89074bb834be8311c3b1e9cbfb0a8a633bec33d9
Matt Vuk <matthewvuk@gmail.com>
2018-03-01 02:14:13 -0500

    company

--- page-templates/company.php
+++ page-templates/company.php
@@ -95,7 +95,131 @@ endwhile;
 				<p>info@e-zn.com</p>
 			</div>
 			<div class="col-md-6">
-				<img src="/wp-content/uploads/2018/02/e-zn-facility.png" alt="e-Zn facility">
+				<img src="/wp-content/uploads/2018/02/e-zn-facility.png" alt="e-Zn facility" class="margin">
+				<div class="googleMap">
+
+					<style>
+						#map {
+							height: 475px;
+							width: 100%;
+						 }
+					</style>
+
+					<div id="map"></div>
+			    <script>
+						var styles = {
+							silver: [
+                //
+								{
+			            elementType: 'geometry',
+			            stylers: [{color: '#f5f5f5'}]
+			          },
+			          {
+			            elementType: 'labels.icon',
+			            stylers: [{visibility: 'off'}]
+			          },
+			          {
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#616161'}]
+			          },
+			          {
+			            elementType: 'labels.text.stroke',
+			            stylers: [{color: '#f5f5f5'}]
+			          },
+			          {
+			            featureType: 'administrative.land_parcel',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#bdbdbd'}]
+			          },
+			          {
+			            featureType: 'poi',
+			            elementType: 'geometry',
+			            stylers: [{color: '#eeeeee'}]
+			          },
+			          {
+			            featureType: 'poi',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#757575'}]
+			          },
+			          {
+			            featureType: 'poi.park',
+			            elementType: 'geometry',
+			            stylers: [{color: '#e5e5e5'}]
+			          },
+			          {
+			            featureType: 'poi.park',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#9e9e9e'}]
+			          },
+			          {
+			            featureType: 'road',
+			            elementType: 'geometry',
+			            stylers: [{color: '#ffffff'}]
+			          },
+			          {
+			            featureType: 'road.arterial',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#757575'}]
+			          },
+			          {
+			            featureType: 'road.highway',
+			            elementType: 'geometry',
+			            stylers: [{color: '#dadada'}]
+			          },
+			          {
+			            featureType: 'road.highway',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#616161'}]
+			          },
+			          {
+			            featureType: 'road.local',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#9e9e9e'}]
+			          },
+			          {
+			            featureType: 'transit.line',
+			            elementType: 'geometry',
+			            stylers: [{color: '#e5e5e5'}]
+			          },
+			          {
+			            featureType: 'transit.station',
+			            elementType: 'geometry',
+			            stylers: [{color: '#eeeeee'}]
+			          },
+			          {
+			            featureType: 'water',
+			            elementType: 'geometry',
+			            stylers: [{color: '#c9c9c9'}]
+			          },
+			          {
+			            featureType: 'water',
+			            elementType: 'labels.text.fill',
+			            stylers: [{color: '#9e9e9e'}]
+			          }
+                //
+							]
+						}
+
+			      function initMap() {
+			        var ezn = {lat: 43.633409, lng: -79.528817};
+			        var map = new google.maps.Map(document.getElementById('map'), {
+			          zoom: 14,
+			          center: ezn,
+								styles : styles.silver,
+								disableDefaultUI: true
+			        });
+			        var marker = new google.maps.Marker({
+			          position: ezn,
+			          map: map
+			        });
+			      }
+			    </script>
+
+					<!--  -->
+					<script async defer
+					src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBL-T-9TVGiW0q-65cAq5dfIIP-_jncPY4&callback=initMap">
+					</script>
+				</div>
 			</div>
 		</div>
 	</div>
