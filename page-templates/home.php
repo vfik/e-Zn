611867398b1ca540b44f19b5f64144e96050c85b
vfik <46942629+vfik@users.noreply.github.com>
2019-10-01 16:58:41 -0400

    reworked home page

--- page-templates/home.php
+++ page-templates/home.php
@@ -34,69 +34,33 @@ endwhile;
 	</div>
 </div> -->
 
-<div class="video-intro">
-	<div class="videoDiv hidden-sm-down">
-		<!-- <video src="/wp-content/uploads/2018/01/video-2018-01-09.mp4" autoplay loop class="videoElement"> -->
-		<video autoplay loop class="videoElement home">
-			<source src="/wp-content/uploads/2018/03/herovidHQslower.mp4" type="video/mp4">
-			<source src="/wp-content/uploads/2018/03/herovidHQslower.webm" type="video/webm">
-		</video>
-	</div>
-
-	<div class="novaNews">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 titleIntro">
-					<h1>Economical Energy Storage</h1>
-				</div>
-			</div>
-		</div>
-	</div>
-
-</div>
 
 <div class="home-content-intro">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">
-				<div class="content">
-					<h2>Lowest cost energy storage technology</h2>
-					<p>e-Zn's unique Zn Reactor Technology turns electricity into physically free zinc material.</p>
-					<p>The metal can be flexibly and inexpensively contained for large scale storage of energy.</p>
-				</div>
-				<img src="/wp-content/uploads/2018/03/stored-zinc-e-zn.jpg" class="" alt style ="padding-bottom:30px;">
-				<!-- <div class="operation-video">
-					<video height="auto" controls="controls" preload="none" onclick="this.play()" class="videoElement" poster="/wp-content/uploads/2018/04/zinc-reactor-video-poster.jpg">
-						<source src="/wp-content/uploads/2018/04/e-zn_zinc_reactor_optimized-export1.mp4" type="video/mp4">
-					</video>
-				</div> -->
-			</div>
-			<div class="col-md-6">
-				<div class="metalized-electricity">
-					<img src="/wp-content/uploads/2018/03/zinc-hands.jpg" alt ="">
+
+			<div class="col-md-12">
+				<div class="container">
+					<div class="content">
+						<img src="/wp-content/uploads/2019/10/50ToWatch_Banner_640X110.jpg" alt style="padding-top:20px; padding-bottom:20px; width:100rem;">
+					</div>
 				</div>
 			</div>
 
-			<div class="col-md-8">
+			<div class="col-md-12">
 				<div class="container">
 					<div>
 						<p style="padding-top: 10px;">e-Zn was chosen to be on the 2019 Cleantech 50 to watch list.</p>
-						<p>Cleantech&reg; Group is an international company focused on supporting innovating clean technologies <a href="http://info.cleantech.com/50-to-Watch-2019-report-download.html?utm_campaign=50%20to%20Watch%2019&utm_medium=links&utm_source=PR_Kit">Download the report</a></p>
+						<p>Cleantech&reg; Group is an international company focused on supporting innovating clean technologies. <a href="https://www.cleantech.com/release/top-companies-delivering-high-impact-solutions-named-in-inaugural-list/">Learn more here.</a></p>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
-				<div class="container">
-					<div class="content">
-						<img src="/wp-content/uploads/2019/10/cleantechlogo.jpg" alt style="padding-top:20px; padding-bottom:20px;">
-					</div>
-				</div>
-			</div>
+
 
 			<div class="homecontent col-md-8">
 				<div class="container">
 					<div class="content">
-						<p style="padding-top: 10px;"> We were interviewed by BNN Bloomberg. <a href="https://www.bnnbloomberg.ca/commodities/video/power-shift-zinc-s-advantages-for-battery-storage-over-lithium-ion~1782308">Watch the video</a></p>
+						<p style="padding-top: 10px;"> We were interviewed by BNN Bloomberg. <a href="https://www.bnnbloomberg.ca/commodities/video/power-shift-zinc-s-advantages-for-battery-storage-over-lithium-ion~1782308">Watch the interview here.</a></p>
 					</div>
 				</div>
 			</div>
@@ -126,6 +90,65 @@ endwhile;
 		</div>
 	</div>
 </div>
+
+
+
+<div class="homecontent col-md-12">
+	<div class="container">
+		<div class="content">
+
+			<div class="video-intro">
+				<div class="videoDiv hidden-sm-down" style = "height:300px;">
+					<!-- keep this out <video src="/wp-content/uploads/2018/01/video-2018-01-09.mp4" autoplay loop class="videoElement"> -->
+					<video  autoplay loop class="videoElement home" >
+						<source src="/wp-content/uploads/2018/03/herovidHQslower.mp4" type="video/mp4">
+						<source src="/wp-content/uploads/2018/03/herovidHQslower.webm" type="video/webm">
+					</video>
+				</div>
+
+
+			</div>
+			<div class="novaNews" style = "height:100px">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 titleIntro">
+							<h1 style = "text-align: center;">Economical Energy Storage</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<div class="home-content-intro">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="content">
+					<h2>Lowest cost energy storage technology</h2>
+					<p>e-Zn's unique Zn Reactor Technology turns electricity into physically free zinc material.</p>
+					<p>The metal can be flexibly and inexpensively contained for large scale storage of energy.</p>
+				</div>
+				<img src="/wp-content/uploads/2018/03/stored-zinc-e-zn.jpg" class="" alt style ="padding-bottom:30px;">
+				<!-- <div class="operation-video">
+					<video height="auto" controls="controls" preload="none" onclick="this.play()" class="videoElement" poster="/wp-content/uploads/2018/04/zinc-reactor-video-poster.jpg">
+						<source src="/wp-content/uploads/2018/04/e-zn_zinc_reactor_optimized-export1.mp4" type="video/mp4">
+					</video>
+				</div> -->
+			</div>
+			<div class="col-md-6">
+				<div class="metalized-electricity">
+					<img src="/wp-content/uploads/2018/03/zinc-hands.jpg" alt ="">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 
 <!-- <div class="container home-con">
 	<div class="row">
